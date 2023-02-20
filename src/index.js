0b9409c6af4caf776706cea4572b13cb35adad61
Volodymyr Vasylkivskyi <vasilksvskij@gmail.com>
2023-02-20 21:22:48 +0200

    feat: add index.js file;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+  const matchingBrackets = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const config = bracketsConfig[i];
+    openingBrackets.push(config[0]);
+    closingBrackets.push(config[1]);
+    matchingBrackets[config[1]] = config[0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (openingBrackets.includes(char)) {
+      if (matchingBrackets[char] !== char) {
+        stack.push(char);
+      } else {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      }
+    } else if (closingBrackets.includes(char)) {
+      const matchingOpeningBracket = matchingBrackets[char];
+      if (stack.length === 0 || stack[stack.length - 1] !== matchingOpeningBracket) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
 }
+
